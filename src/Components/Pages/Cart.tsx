020fb2a8e537478394e962e9569608d6cd66db4d
AndrewLin95 <andrew.lin88@hotmail.com>
2022-08-01 12:18:49 -0400

    added logic to update cart and update quantity of each item in the cart

--- src/Components/Pages/Cart.tsx
+++ src/Components/Pages/Cart.tsx
@@ -1,7 +1,31 @@
+import { FC } from 'react';
 
-const Cart = () => {
+
+interface Obj{
+    description: string,
+    price: number,
+    img: string,
+    quantity: number
+}
+
+interface Props{
+    [key: string]: any
+}
+
+const Cart:FC<Props> = ( cart ) => {
+
     return (
-        <div>CART</div>
+        <div>  
+            {Object.entries(cart).map(([key, value]) => {
+                return(
+                    <div>
+                        Test
+                    </div>
+                )
+            })
+
+            }
+        </div>
     )
 }
 
--- src/RouteSwitch.tsx
+++ src/RouteSwitch.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import Home from "./Components/Pages/Home";
 import Products from "./Components/Pages/Products";
 import Contacts from "./Components/Pages/Contacts";
@@ -18,40 +18,45 @@ interface Obj{
 
 const RouteSwitch:FC = () => {
 
-    const [cart, useCart] = useState({});
+    const [cart, useCart] = useState<{[key: string]: any}>({});
     const [price, usePrice] = useState();
 
     // 1) have cart show each item. Each item to include picture, price of each, description and quantity
     // 2) show sum of prices at the bottom. Can use a separate state for this
 
     const updateCart = ( descriptionValue: string, priceValue: number, image: string, id: number ) => {
-        const tempCart = cart;
-        const obj: {[id: number] : Obj } = {};  
-        obj[id] = {
-            description: descriptionValue,
-            price: priceValue,
-            img: image,
-            quantity: 1
-        };
+        const tempCart: {[id: number] : Obj} = cart;
+        console.log(tempCart);
         if (id in tempCart){
-            console.log(obj[id].quantity)
+            tempCart[id].quantity ++;
+            useCart(tempCart);
+        } else {
+            tempCart[id] = {
+                description: descriptionValue,
+                price: priceValue,
+                img: image,
+                quantity: 1
+            };
+            useCart(tempCart);
         }
-        useCart(obj);
-        console.log(cart);
     }
+
+    useEffect(()=>{
+        console.log(cart);
+    }, [cart])
 
     return (
         <>
-            <Header/>
             <BrowserRouter>
+            <Header/>
                 <Routes> 
                     <Route path="/" element={<Home/>} />
                     <Route path="/products" element={<Products updateCart={updateCart}/>} />
                     <Route path="/contacts" element={<Contacts/>} />
-                    <Route path="/cart" element={<Cart/>}/>
+                    <Route path="/cart" element={<Cart cart={cart}/>}/>
                 </Routes>
-            </BrowserRouter>
             <Footer/>
+            </BrowserRouter>
         </>
     )
 }
